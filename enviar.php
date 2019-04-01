303f9d307bd95780ebf0f84737948de7e94a3ac9
Andrea Carreño <45242344+AndreaVCC@users.noreply.github.com>
2019-04-01 14:40:43 -0300

    modificando php

--- enviar.php
+++ enviar.php
@@ -1,19 +1,6 @@
 <?php
 
-<?php
-$nombre = $_POST['nombre'];
-$asunto = $_POST['email'];
-$mensaje = $_POST['mensaje'];
 
-echo "<h2>Informacion recibida desde PHP</h2>";
-echo "El nombre recibido es: " . $nombre . "<br/>";
-echo "El asunto recibido es: " . $asunto . "<br/>";
-echo "El mensaje recibido es: " . $mensaje . "<br/>";
-
-?>
-
-
-/*e crea variable y le da el valor que tnga el input
 
 $nombre = $_POST['nombre'];
 $mail = $_POST['mail'];
@@ -36,7 +23,6 @@ $asunto = 'Mensaje de mi sitio web';
 if (mail($para, $asunto, utf8_decode($mensaje), $header))
 echo "<script type='text/javascript'>alert('Tu mensaje ha sido enviado exitosamente');</script>";
 echo "<script type='text/javascript'>window.location.href='http://bluuweb.cl/plantilla-1/index.html';</script>";
-*/
 
 
 
