27cd1fa4a8075c4e3c56a2c0b7960bdbcf693870
Andrea Carreño <45242344+AndreaVCC@users.noreply.github.com>
2019-03-31 22:12:16 -0300

    actualizando

---
+++ enviar.php
@@ -0,0 +1,46 @@
+<?php
+
+header("Content-type: text/html;charset=\"utf-8\"");
+
+$myemail = 'a.carreocoppo2@gmail.com';
+$name = $_POST['nombre'];
+$email = $_POST['email'];
+$message = $_POST['mensaje'];
+
+$to = $myemail;
+$email_subject = "Nuevo mensaje: $subject";
+$email_body = "Haz recibido un nuevo mensaje. \n Nombre: $name \n Correo: $email \n Mensaje: \n $message";
+$headers = "From: $email";
+
+mail($to, $email_subject, $email_body, $headers);
+echo "El mensaje se ha enviado correctamente";
+
+
+/*e crea variable y le da el valor que tnga el input
+
+$nombre = $_POST['nombre'];
+$mail = $_POST['mail'];
+$mensaje = $_POST['mensaje'];
+
+$header = 'From: ' . $mail . " \r\n";
+$header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
+$header .= "Mime-Version: 1.0 \r\n";
+$header .= "Content-Type: text/plain";
+
+$mensaje = "Este mensaje fue enviado por " . $nombre . ",\r\n";
+$mensaje .= "Su e-mail es: " . $mail . " \r\n";
+$mensaje .= "Texto: " . $mensaje . " \r\n";
+
+$mensaje .= "Enviado el " . date('d/m/Y', time());
+
+$para = 'a.carreocoppo2@gmail.com';
+$asunto = 'Mensaje de mi sitio web';
+
+if (mail($para, $asunto, utf8_decode($mensaje), $header))
+echo "<script type='text/javascript'>alert('Tu mensaje ha sido enviado exitosamente');</script>";
+echo "<script type='text/javascript'>window.location.href='http://bluuweb.cl/plantilla-1/index.html';</script>";
+*/
+
+
+
+?>
