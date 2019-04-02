d8752b507df5ad5cbba23f17635884e389d5aacf
Andrea Carreño <45242344+AndreaVCC@users.noreply.github.com>
2019-04-02 03:19:03 -0300

    Probando parpadeo

--- src/index.js
+++ src/index.js
@@ -1,29 +1,6 @@
-/*
-document.getElementById("idBtnEnviar").addEventListener("click",function(){
-    const strNombre = document.getElementById("nombre").value;
-    const strEmail = document.getElementById("email").value;
-    const strTexto = document.getElementById("texto").value;
 
-    if(strNombre != "" && strEmail != "" && strTexto != ""){
-        const datos ={
-            n: strNombre,
-            e: strEmail,
-            t: strTexto
-        }
-
-
-        
-    }else{
-        alert("Rellena los campos");
-    }
-});
-*/
-
-var iconElement = document.getElementById('icon');
-var options = {
-    from: 'fa-bars',
-    to: 'fa-arrow-right',
-    animation: 'rubberBand'
-};
-
-iconate(iconElement, options);
+const animacion = function(){
+      document.getElementById('formulario').classList.toggle('fade');
+  }
+  
+  setInterval(animacion, 1400);
